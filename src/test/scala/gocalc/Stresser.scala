030d5c1a413e82cfff78f73c577f05e7fd1186ba
Magalhães <arfddem@gmail.com>
2019-08-28 10:23:58 -0300

    Changed Stresser structure modeling objects for each request, following POM pattern. Started adding external file feeding.

--- src/test/scala/gocalc/Stresser.scala
+++ src/test/scala/gocalc/Stresser.scala
@@ -6,47 +6,65 @@ import scala.concurrent.duration._
 
 class Stresser extends Simulation{
 
-   val httpProtocol = http.baseUrl("http://localhost:8083").acceptHeader("application/json")
-
-    val scenery = scenario("History Stressing")
-        .exec(http("Sum #1")
+    val feeder = csv("stresserData.csv").eager.random
+    val httpProtocol = http.baseUrl("http://localhost:8083").acceptHeader("application/json")
+    
+    object Sum {
+        val execute = exec(http("Sum #1")
             .get("/calc/sum/1/2"))
+            .pause(2)
         .exec(http("Sum #2")
             .get("/calc/sum/3/4"))
+            .pause(2)
         .exec(http("Sum #3")
             .get("/calc/sum/7/8"))
-        .pause(1000 milliseconds)
-        .exec(http("Acessing history")
-            .get("/calc/history"))
+            .pause(2)
+        .exec(http("History Endpoint")
+            .get("/calc/history"))        
+    }
 
-        .exec(http("Sub #1")
+    object Sub {
+        val execute = exec(http("Sub #1")
             .get("/calc/sub/1/2"))
+            .pause(3)
         .exec(http("Sub #2")
             .get("/calc/sub/3/4"))
+            .pause(2)
         .exec(http("Sub #3")
             .get("/calc/sub/7/8"))
-        .pause(1000 milliseconds)
-        .exec(http("Acessing history")
+            .pause(2)
+        .exec(http("History Endpoint")
             .get("/calc/history"))
+    }
 
-        .exec(http("Mul #1")
+    object Mul {
+        val execute = exec(http("Mul #1")
             .get("/calc/multiply/1/2"))
+            .pause(1)
         .exec(http("Mul #2")
             .get("/calc/multiply/3/4"))
+            .pause(1)
         .exec(http("Mul #3")
             .get("/calc/multiply/7/8"))
-        .pause(1000 milliseconds)
-        .exec(http("Acessing history")
+            .pause(2)
+        .exec(http("History Endpoint")
             .get("/calc/history"))
+    }
 
-        .exec(http("Div #1")
+    object Div {
+        val execute = exec(http("Div #1")
             .get("/calc/division/7/2"))
+            .pause(2)
         .exec(http("Div #2")
             .get("/calc/division/10/4"))
+            .pause(2)
         .exec(http("Division #3")
             .get("/calc/sum/20/8"))
-        .pause(1000 milliseconds)
-        .exec(http("Acessing history")
+            .pause(2)
+        .exec(http("History Endpoint")
             .get("/calc/history"))
-    setUp(scenery.inject(constantUsersPerSec(3).during(60 seconds)).protocols(httpProtocol))
+    }        
+    
+    val scn = scenario("Simple Stressing").exec(Sum.execute, Sub.execute, Div.execute, Mul.execute)    
+    setUp(scn.inject(constantUsersPerSec(3).during(60 seconds)).protocols(httpProtocol))
 }
